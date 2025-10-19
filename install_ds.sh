f2246dd9acfdd137194c007a6f737f6fbed98d97
sader <sader@jrs>
2025-10-19 22:15:37 +0200

    do lg

--- install_ds.sh
+++ install_ds.sh
@@ -16,9 +16,9 @@ TARGET_DIR="data/raw"
 
 mkdir -p "$TARGET_DIR"
 
-unzip -o ./raw/train_dictionary.zip -d  "$TARGET_DIR"
-unzip -o ./raw/traindev.zip -d "$TARGET_DIR"
-unzip -o ./raw/test.zip -d "$TARGET_DIR"
+unzip -o ./raw/train_dictionary_lg.zip -d  "$TARGET_DIR"
+unzip -o ./raw/traindev_lg.zip -d "$TARGET_DIR"
+# unzip -o ./raw/test.zip -d "$TARGET_DIR"
 
 
 
